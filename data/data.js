4f23fc3259d8f361e819acb7efa778fd14dc8796
LeeU1230 <name10831@gmail.com>
2020-11-27 11:51:36 +0800

    'update'

---
+++ data/data.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAlZ7gsMKEVOxCkzWDsQGLxbQ8fm6mt8Zw",
+    authDomain: "test-creat-1ddb9.firebaseapp.com",
+    databaseURL: "https://test-creat-1ddb9.firebaseio.com",
+    projectId: "test-creat-1ddb9",
+    storageBucket: "test-creat-1ddb9.appspot.com",
+    messagingSenderId: "294987352423",
+    appId: "1:294987352423:web:eb162baad1ebe2672dc7c2",
+    measurementId: "G-QHJS638VN1"
+};
